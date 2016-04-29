bd765c87c9bfb913840462fc8957bd28bb0bfe69
J. Santos <jamillo@gmail.com>
2016-04-29 07:29:47 -0300

    Inicio da aplicação e configuração do actuator #1
    * Servidor HTTP rodando na porta 7070;
    * Configuração usando YAML;
    * Mapeamento do endpoint `shutdown` do `Actuator` para `/sair` usando autenticação `http basic`;

---
+++ src/main/java/br/edu/ifrn/dsc/PostoLegalApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifrn.dsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostoLegalApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(PostoLegalApplication.class, args);
+	}
+}
