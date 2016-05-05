309b348a507557d23420116068f5e56c4ae02ecf
JulianSansat <kerarchie@yahoo.com.ar>
2016-05-05 18:36:52 -0300

    Instalado o lombok, implementado a classe user e vehicle #4

---
+++ src/main/java/br/edu/ifrn/postolegal/dominio/Vehicle.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifrn.postolegal.dominio;
+
+import java.io.Serializable;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Builder;
+/**
+ *
+ * @author jbull
+ */
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@Builder
+public class Vehicle implements Serializable{
+    private int year;
+    private String engine;
+    private String model;
+    private User user;
+}
