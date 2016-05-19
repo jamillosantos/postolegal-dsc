258ea165528ebb38ce64ffdf1145b6a095132f1d
JulianSansat <kerarchie@yahoo.com.ar>
2016-05-19 17:23:02 -0300

    Implementado o teste de Station, testando método Equals e compareTo

--- src/test/java/br/edu/ifrn/postolegal/dominio/StationTest.java
+++ src/test/java/br/edu/ifrn/postolegal/dominio/StationTest.java
@@ -18,10 +18,23 @@ import org.testng.annotations.Test;
 @Test
 public class StationTest {
     private static final String NOME = "postoLegal";
+    private static final String ENDERECO = "rua";
+    private static final String CNPJ = "01634555333";
     
     public void nomeIgual() {
         assertThat(Station.builder().name(NOME).build())
             .isEqualTo(Station.builder().name(NOME).build());
+    }
+    
+    public void compareTo() {
+        Set<Station> stations = new TreeSet<>();
+        
+        Station endereco = Station.builder().name(ENDERECO).build();
+        stations.add(endereco);
+        Station cnpj = Station.builder().name(CNPJ).build();
+        stations.add(cnpj);
+        
+        assertThat(stations.iterator().next()).isEqualTo(cnpj);
     }
     
     // TODO add test methods here.
