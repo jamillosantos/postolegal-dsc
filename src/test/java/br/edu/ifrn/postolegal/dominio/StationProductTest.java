2accc20283dc95c3379a57e16e51afa73d3edcfa
J. Santos <jamillo@gmail.com>
2016-05-19 18:20:48 -0300

    Substituindo espaços por tab #6

--- src/test/java/br/edu/ifrn/postolegal/dominio/StationProductTest.java
+++ src/test/java/br/edu/ifrn/postolegal/dominio/StationProductTest.java
@@ -7,32 +7,36 @@ package br.edu.ifrn.postolegal.dominio;
 
 import java.util.Set;
 import java.util.TreeSet;
+
 import static org.assertj.core.api.Assertions.assertThat;
+
 import org.testng.annotations.Test;
 
 /**
- *
  * @author jbull
  */
-public class StationProductTest {
-    private static final long ID = 12;
-    private static final long CODIGO = 1254343;
-    private static final long SENHA = 12345;
-    
-    public void idIgual() {
-        assertThat(StationProduct.builder().productId(ID).build())
-            .isEqualTo(StationProduct.builder().productId(ID).build());
-    }
-    
-    public void compareTo() {
-        Set<StationProduct> stationProducts = new TreeSet<>();
-        
-        StationProduct codigo = StationProduct.builder().productId(CODIGO).build();
-        stationProducts.add(codigo);
-        StationProduct senha = StationProduct.builder().productId(SENHA).build();
-        stationProducts.add(senha);
-        
-        assertThat(stationProducts.iterator().next()).isEqualTo(senha);
-    }
-    
+public class StationProductTest
+{
+	private static final long ID = 12;
+	private static final long CODIGO = 1254343;
+	private static final long SENHA = 12345;
+
+	public void idIgual()
+	{
+		assertThat(StationProduct.builder().productId(ID).build())
+			.isEqualTo(StationProduct.builder().productId(ID).build());
+	}
+
+	public void compareTo()
+	{
+		Set<StationProduct> stationProducts = new TreeSet<>();
+
+		StationProduct codigo = StationProduct.builder().productId(CODIGO).build();
+		stationProducts.add(codigo);
+		StationProduct senha = StationProduct.builder().productId(SENHA).build();
+		stationProducts.add(senha);
+
+		assertThat(stationProducts.iterator().next()).isEqualTo(senha);
+	}
+
 }
--- src/test/java/br/edu/ifrn/postolegal/dominio/StationTest.java
+++ src/test/java/br/edu/ifrn/postolegal/dominio/StationTest.java
@@ -7,38 +7,42 @@ package br.edu.ifrn.postolegal.dominio;
 
 import java.util.Set;
 import java.util.TreeSet;
+
 import static org.assertj.core.api.Assertions.assertThat;
+
 import org.testng.annotations.Test;
 
 /**
- *
  * @author jbull
  */
 @Test
-public class StationTest {
-    private static final String NOME = "postoLegal";
-    private static final String ENDERECO = "rua";
-    private static final String CNPJ = "01634555333";
-    
-    public void nomeIgual() {
-        assertThat(Station.builder().name(NOME).build())
-            .isEqualTo(Station.builder().name(NOME).build());
-    }
-    
-    public void compareTo() {
-        Set<Station> stations = new TreeSet<>();
-        
-        Station endereco = Station.builder().name(ENDERECO).build();
-        stations.add(endereco);
-        Station cnpj = Station.builder().name(CNPJ).build();
-        stations.add(cnpj);
-        
-        assertThat(stations.iterator().next()).isEqualTo(cnpj);
-    }
-    
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+public class StationTest
+{
+	private static final String NOME = "postoLegal";
+	private static final String ENDERECO = "rua";
+	private static final String CNPJ = "01634555333";
+
+	public void nomeIgual()
+	{
+		assertThat(Station.builder().name(NOME).build())
+			.isEqualTo(Station.builder().name(NOME).build());
+	}
+
+	public void compareTo()
+	{
+		Set<Station> stations = new TreeSet<>();
+
+		Station endereco = Station.builder().name(ENDERECO).build();
+		stations.add(endereco);
+		Station cnpj = Station.builder().name(CNPJ).build();
+		stations.add(cnpj);
+
+		assertThat(stations.iterator().next()).isEqualTo(cnpj);
+	}
+
+	// TODO add test methods here.
+	// The methods must be annotated with annotation @Test. For example:
+	//
+	// @Test
+	// public void hello() {}
 }
