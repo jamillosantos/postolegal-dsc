5ef61c7751fcff7e9cad4de32fa5d863012adc91
J. Santos <jamillo@gmail.com>
2016-07-22 00:34:43 -0300

    Implementação do gráfico do histórico

--- src/main/java/br/edu/ifrn/postolegal/view/crud/StationProductHistoryCrudMBean.java
+++ src/main/java/br/edu/ifrn/postolegal/view/crud/StationProductHistoryCrudMBean.java
@@ -5,6 +5,10 @@ import br.edu.ifrn.postolegal.domain.StationProductHistory;
 import br.edu.ifrn.postolegal.service.StationProductHistoryService;
 import br.edu.ifrn.postolegal.service.StationProductService;
 import lombok.Getter;
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.BarChartModel;
+import org.primefaces.model.chart.ChartSeries;
 
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
@@ -21,6 +25,9 @@ public class StationProductHistoryCrudMBean extends CrudMBean<StationProductHist
 
 	private StationProductService _stationProductService;
 
+	@Getter
+	private BarChartModel chart;
+
 	@Override
 	protected void init()
 	{
@@ -32,6 +39,29 @@ public class StationProductHistoryCrudMBean extends CrudMBean<StationProductHist
 		{
 			this.stationProduct = null;
 		}
+
+		this.chart = new BarChartModel();
+
+		ChartSeries prices = new ChartSeries();
+		prices.setLabel("Preços");
+		float maxValue = 0;
+		for (StationProductHistory h : this.find())
+		{
+			prices.set(h.getDate(), h.getPrice());
+			if (maxValue < h.getPrice())
+				maxValue = h.getPrice();
+		}
+		this.chart.setTitle("Histórico de preço");
+		this.chart.setLegendPosition("ne");
+
+		Axis xAxis = this.chart.getAxis(AxisType.X);
+		xAxis.setLabel("Data");
+		Axis yAxis = this.chart.getAxis(AxisType.Y);
+		yAxis.setLabel("Preço");
+		yAxis.setMin(0);
+		yAxis.setMax(maxValue + 0.25);
+
+		this.chart.addSeries(prices);
 	}
 
 	@Inject
